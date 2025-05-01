ee410e2139b010cb85fecad47fe06525e930c4cc
med oussema boussida <medoussemaboussida@gmail.com>
2025-05-01 20:22:37 +0100

    dist

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/oussema-portfolio/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
